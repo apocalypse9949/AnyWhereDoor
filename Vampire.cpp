c4e37abd66218dc5774bdd3d4642f62686e60580
apocalypse9949 <125962989+apocalypse9949@users.noreply.github.com>
2024-09-28 11:25:50 +0530

    Update Vampire.cpp

--- Vampire.cpp
+++ Vampire.cpp
@@ -18,9 +18,9 @@ private:
 public:
     // Constructor to initialize the network address
     Vampire(const std::string& network_address) : network(network_address) {}
-               std::vector<std::string> Dracula_creation(){
+               std::vector<std::string> Dracula_creation(){ // creating new network address
                    std::vector<std::string> addresses;
                    std::istringstream ss(network);
-
+                   std::
                    
              
